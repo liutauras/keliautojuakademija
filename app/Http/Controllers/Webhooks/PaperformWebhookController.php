13a1e2ffd99bc590a1c7a27acd0e40d4f119f9be
Liutauras Valiukas <v.liutauras@gmail.com>
2021-06-17 15:13:18 +0300

    work in progress         modified:   app/Http/Controllers/Webhooks/PaperformWebhookController.php         modified:   app/Http/Services/PaperformService.php

--- app/Http/Controllers/Webhooks/PaperformWebhookController.php
+++ app/Http/Controllers/Webhooks/PaperformWebhookController.php
@@ -16,39 +16,21 @@ class PaperformWebhookController extends Controller
     //
     public function handle(Request $request, $url)
     {
-        $data = $request->data;
-        //$paperformde = new PaperformDataExtractor();
-        // move to service class
         $paperform = Paperform::where('url', $url)->get()->first();
-        // $key_name = [
-        //     ['title' => 'Vardas',       'model_param' => 'vardas'],
-        //     ['title' => 'Telefonas',    'model_param' => 'tel'],
-        //     ['title' => 'El. paštas',   'model_param' => 'el_pastas'],
-        //     ['title' => 'URL',          'model_param' => 'puslapis'],
-        // ];
         
         if ($paperform) {
-        
-            $uzklausa = new Uzklausa();
-            $paperformService = new PaperformServices();
-            $uzklausa = $paperformService->setUzklausaModelData($uzklausa, $data);
-        
-            // foreach($data as $data_item) {
-            //     if(is_array($data_item)) {
-            //         foreach($key_name as $kn) {
-            //             if($data_item['title'] == $kn['title']) {
-            //                 $uzklausa->{$kn['model_param']} = $data_item['value'];
-            //             }
-            //         }
-            //     }
-            // }
-            // $uzklausa->puslapis = $url;
+            
+            $data               = $request->data;
+            $uzklausa           = new Uzklausa();
+            $paperformService   = new PaperformServices();
+            $uzklausa           = $paperformService->setUzklausaModelData($uzklausa, $data);
+            
             $uzklausa->uzklausa = serialize($request->data);
             $uzklausa->save();
+            
+            return 'OK';
         } else {
             return 'BAD';
         }
-        
-        return 'OK';
     }
 }
